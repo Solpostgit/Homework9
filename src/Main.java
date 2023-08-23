e1175655d50e9b771b74aac7436976f8acf2a50e
Solpostgit <begemot_off@mail.ru>
2023-08-23 22:44:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        task1();/*
+        task2();
+        task3();
+        task4();*/
+
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void task1() {
+        System.out.println("Task 1");
+        int[] arr = generateRandomArray();
+
+    }
+}
