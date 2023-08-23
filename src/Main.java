b0348536da90e2e519fbf8b218f333e81b319ac4
Solpostgit <begemot_off@mail.ru>
2023-08-23 23:28:56 +0300

    Added task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        task1();/*
-        task2();
+        task1();
+        task2();/*
         task3();
         task4();*/
 
@@ -21,9 +21,28 @@ public class Main {
         System.out.println("Task 1");
         int[] arr = generateRandomArray();
         int sum = 0;
-        for (int element : arr){
-            sum+=element;
+        for (int element : arr) {
+            sum += element;
         }
         System.out.printf("Сумма трат за месяц составила %d рублей\n", sum);
+    }
+
+    public static void task2() {
+        System.out.println();
+        System.out.println("Task 2");
+        int[] arr = generateRandomArray();
+        int minPay = 300000;
+        int maxPay = 0;
+        for (final int current : arr) {
+            if (current < minPay) {
+                minPay = current;
+            }
+        }
+        for (final int current : arr) {
+            if (current > maxPay) {
+                maxPay = current;
+            }
+        }
+        System.out.printf("Минимальная сумма трат за день составила %d рублей. Максимальная сумма трат за день составила %d рублей \n", minPay, maxPay);
     }
 }
